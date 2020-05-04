01b71ec88b67f961b3cebca46b56554c7e154f1d
Vladimiras <vladimiras.petrovas@gmail.com>
2020-05-04 15:43:31 +0300

    template fixes for mobile version

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,38 +8,34 @@
 
     
         @isset($results)
-            <div class="container px-5">
-                <div class="row justify-content-center mb-3">
-                    <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+            <div class="d-flex justify-content-center mb-3 mx-2">
+                <a href="{{ $id-1 }}">
+                    <button type="button" class="btn btn-outline-dark mr-1">
+                        <span aria-hidden="true">&laquo;</span>
+                    </button>
+                </a>
 
-                        <a href="{{ $id-1 }}">
-                            <button type="button" class="btn btn-outline-dark mr-1">
-                                <span aria-hidden="true">&laquo;</span>
-                            </button>
-                        </a>
-
-                        <div class="btn-group mr-3 mb-2" role="group" aria-label="Third group">
-                            <a href="1"><button type="button" class="btn btn-outline-dark {{ $id == 1 ? 'active' : '' }}">1</button></a>
-                        </div>
-                        <div class="btn-group mr-3 mb-2" role="group" aria-label="Second group">
-                            @for ($i=$id-6; $i<=$id+6; $i++)
-                                @if ($i >= 1 && $i <=1000000-1 && $i != 1)
-                                    <a href="{{ $i }}"><button type="button" class="btn btn-outline-dark mr-1 {{ $id == $i ? 'active' : '' }}">{{ $i }}</button></a>
-                                @endif
-                            @endfor
-                        </div>
-                        <div class="btn-group mb-2" role="group" aria-label="Third group">
-                            <a href="1000000"><button type="button" class="btn btn-outline-dark {{ $id == 1000000 ? 'active' : '' }}">1000000</button></a>
-                        </div>
-
-                        <a href="{{ $id+1 }}">
-                            <button type="button" class="btn btn-outline-dark ml-1">
-                                <span aria-hidden="true">&raquo;</span>
-                            </button>
-                        </a>
-                        
-                    </div>
+                <div class="mb-2">
+                    <a href="1"><button type="button" class="btn btn-outline-dark {{ $id == 1 ? 'active' : '' }}">1</button></a>
                 </div>
+
+                <div class="row justify-content-center mx-3 mb-2">
+                    @for ($i=$id-5; $i<=$id+5; $i++)
+                        @if ($i >= 1 && $i <=1000000-1 && $i != 1)
+                            <a href="{{ $i }}"><button type="button" class="btn btn-outline-dark mr-1 {{ $id == $i ? 'active' : '' }}">{{ $i }}</button></a>
+                        @endif
+                    @endfor
+                </div>
+
+                <div class="mb-2">
+                    <a href="1000000"><button type="button" class="btn btn-outline-dark {{ $id == 1000000 ? 'active' : '' }}">1000000</button></a>
+                </div>
+
+                <a href="{{ $id+1 }}">
+                    <button type="button" class="btn btn-outline-dark ml-1">
+                        <span aria-hidden="true">&raquo;</span>
+                    </button>
+                </a>
             </div>
 
             <div class="row justify-content-center mb-3">
@@ -52,28 +48,36 @@
                 </div>
             </div>
         @else
-            <div class="container px-5">
-                <div class="row justify-content-center mb-3">
-                    <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-                        @php
-                            $i = 1;
-                        @endphp
-                        <button type="button" class="btn btn-outline-dark mr-1 mb-2" disabled>
-                            <span aria-hidden="true">&laquo;</span>
-                        </button>
-                        
-                        @for ($i=1; $i<=10; $i++)
-                            <a href="{{ $i }}"><button type="button" class="btn btn-outline-dark mr-1 mb-2">{{ $i }}</button></a>
-                        @endfor
-                        <a href="1000000"><button type="button" class="btn btn-outline-dark mb-2">1000000</button></a>
-                        
-                        <a href="{{ $i }}">
-                            <button type="button" class="btn btn-outline-dark ml-1 mb-2">
-                                <span aria-hidden="true">&raquo;</span>
-                            </button>
-                        </a>
-                    </div>
+            <div class="d-flex justify-content-center mb-3 mx-2">
+                @php
+                    $i = 1;
+                @endphp
+
+                <a>
+                    <button type="button" class="btn btn-outline-dark mr-1" disabled>
+                        <span aria-hidden="true">&laquo;</span>
+                    </button>
+                </a>
+
+                <div class="mb-2">
+                    <a href="1"><button type="button" class="btn btn-outline-dark">1</button></a>
                 </div>
+                
+                <div class="row justify-content-center mx-3 mb-2">
+                    @for ($i=1; $i<=10; $i++)
+                        <a href="{{ $i }}"><button type="button" class="btn btn-outline-dark mr-1 mb-2">{{ $i }}</button></a>
+                    @endfor
+                </div>
+
+                <div class="mb-2">
+                    <a href="1000000"><button type="button" class="btn btn-outline-dark mb-2">1000000</button></a>
+                </div>
+
+                <a href="{{ $i }}">
+                    <button type="button" class="btn btn-outline-dark ml-1 mb-2">
+                        <span aria-hidden="true">&raquo;</span>
+                    </button>
+                </a>
             </div>
         @endisset
 
